5b5b3cbb61c0fa7a3114afa6d9bbb699ae74a145
Maksim Baklykov <maximbaklykov@yandex.com>
2023-05-25 18:54:04 +0500

    base test commit

--- src/test/java/ru/praktikum/selenium/BaseTest.java
+++ src/test/java/ru/praktikum/selenium/BaseTest.java
@@ -5,16 +5,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 
 public class BaseTest {
     WebDriver driver;
     @Before
 
     public void startUp() {
-        WebDriverManager.firefoxdriver().clearDriverCache().setup();
-        driver = new FirefoxDriver();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     @After
